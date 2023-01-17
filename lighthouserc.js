2092cce25db53e8a458ef2650505c8b3ed929525
apo1798 <nploxz@gmail.com>
2023-01-17 18:16:11 +0800

    🔧 attempt to run lhci #3

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,17 @@
 module.exports = {
   ci: {
-    upload: {
-      target: 'temporary-public-storage',
+    assert: {
+      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': [
+          'warn',
+          { aggregationMethod: 'optimistic', minScore: 0.93 },
+        ],
+        'categories:accessibility': [
+          'error',
+          { aggregationMethod: 'optimistic', minScore: 0.95 },
+        ],
+      },
     },
   },
 };
