b0b213f3663ecd99137c83bac1015ac47ddd74b3
apo1798 <nploxz@gmail.com>
2023-01-17 18:30:03 +0800

    revert back lh settings

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,17 +1,7 @@
 module.exports = {
   ci: {
-    assert: {
-      preset: 'lighthouse:no-pwa',
-      assertions: {
-        'categories:performance': [
-          'warn',
-          { aggregationMethod: 'optimistic', minScore: 0.93 },
-        ],
-        'categories:accessibility': [
-          'error',
-          { aggregationMethod: 'optimistic', minScore: 0.95 },
-        ],
-      },
+    upload: {
+      target: 'temporary-public-storage',
     },
   },
 };
